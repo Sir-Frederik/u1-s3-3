e2669b16e3024573468c5ebb5f7ca9458ef26858
Federico Brunetti <brunetti.federico.13@gmail.com>
2025-03-26 23:17:30 +0100

    lavorando seguendo il video. creando il form

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-window.onload = function () {
-  const body = document.querySelector("body");
+// window.onload = function () {
+//   const body = document.querySelector("body");
 
-  const form = document.createElement("form");
-  const input = document.createElement("input");
-  const button = document.createElement("button");
+//   const form = document.createElement("form");
+//   const input = document.createElement("input");
+//   const button = document.createElement("button");
 
-  button.textContent = "Aggiungi";
-  input.placeholder = "Nuova Task";
+//   button.textContent = "Aggiungi";
+//   input.placeholder = "Nuova Task";
 
-  form.appendChild(input);
-  form.appendChild(button);
-  body.appendChild(form);
+//   form.appendChild(input);
+//   form.appendChild(button);
+//   body.appendChild(form);
+const form = document.querySelector("form");
+const list = document.getElementById("tasksList");
 
-const handleBtnClick = function (e){
-    console.log ("cliccato");
-}
+form.onsubmit = function (event) {
+  event.preventDefault();
+  console.log("submit");
 
-  const div = document.createElement("div");
-  const ul = document.createElement("ul");
-
-  div.id = "divTasks";
-  body.appendChild(div);
+  const inputNode = document.getElementById("newTask");
+  console.log(inputNode.value);
 };
 
 console.log("ciao");
